01f0e34ae8002728c514ecfc9f01e2b0d70dac98
b-rd <88917909+b-rd@users.noreply.github.com>
2021-08-13 20:43:19 -0700

    initial commit

---
+++ skin.php
@@ -0,0 +1,37 @@
+<?php
+// Aluminum/skin.php
+// Aluminum skin file.
+
+if (!defined("IN_ESO")) exit;
+
+class EsoFlat extends Skin {
+
+var $name = "Flat";
+var $version = "0.1.0";
+var $author = "berd";
+var $numberOfColors = 27;
+
+// Add stylesheets and a favicon to the page header.
+function init()
+{
+	global $config;
+	$this->eso->addCSS("skins/{$config["skin"]}/styles.css");
+	$this->eso->addToHead("<link rel='shortcut icon' type='image/ico' href='skins/{$config["skin"]}/favicon.ico'/>");
+}
+
+// Generate button HTML.
+function button($attributes)
+{
+	$class = $id = $style = ""; $attr = " type='submit'";
+	foreach ($attributes as $k => $v) {
+		if ($k == "class") $class = " $v";
+		elseif ($k == "id") $id = " id='$v'";
+		elseif ($k == "style") $style = " style='$v'";
+		else $attr .= " $k='$v'";
+	}
+	return "<span class='button$class'$id$style><input$attr/></span>";
+}
+
+}
+
+?>
